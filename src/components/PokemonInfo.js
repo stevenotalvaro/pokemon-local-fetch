c193df07ad447603d5ba8d89bbdfef8e3c4ffbed
Steven Otalvaro <stevenotalvaroz@gmail.com>
2021-06-28 11:15:40 -0500

    Set Error, implemented

--- src/components/PokemonInfo.js
+++ src/components/PokemonInfo.js
@@ -4,18 +4,27 @@ import {PokemonDataView} from './PokemonDataView'
 import {PokemonInfoFallback} from './PokemonInfoFallback'
 export function PokemonInfo({pokemonName}) {
   const [pokemon, setPokemon] = useState(null)
-
+  const [error, setError] = useState(null)
   useEffect(() => {
     if (!pokemonName) {
       return
     }
-    setPokemon(null)
-    fetchPokemon(pokemonName).then(pokemon => {
-      setPokemon(pokemon)
-    })
+    setPokemon(null) // when te state change, setPokemon will again null for load the new pokemon
+    setError(null) // when te state change, setError will again null for load the new pokemon
+    fetchPokemon(pokemonName).then(
+      pokemon => setPokemon(pokemon),
+      error => setError(error),
+    )
   }, [pokemonName])
-
-  if (!pokemonName) {
+  if (error) {
+    // this validation will executed when the name pokemon not exit
+    return (
+      <div role="alert">
+        There was an error:{' '}
+        <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+      </div>
+    )
+  } else if (!pokemonName) {
     return 'Submit a pokemon' // when it load to first screen,  there isn't pokemon, so to show this message
   } else if (!pokemon) {
     return <PokemonInfoFallback name={pokemonName} /> //when the request is being made, it shower theimg load
