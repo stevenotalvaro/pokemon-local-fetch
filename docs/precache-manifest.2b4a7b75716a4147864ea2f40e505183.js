3ee1436915399842e70514725b2bbc113c5e7eed
Steven Otalvaro <stevenotalvaroz@gmail.com>
2021-06-28 12:46:57 -0500

    docs push

---
+++ docs/precache-manifest.2b4a7b75716a4147864ea2f40e505183.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9838ba2115ecc8be0aa8052aa60d1b43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc4c2a06268159375ec1",
+    "url": "/static/css/main.0413cc93.chunk.css"
+  },
+  {
+    "revision": "4f8add5bb22ff76969ea",
+    "url": "/static/js/2.8cbb7d59.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.8cbb7d59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc4c2a06268159375ec1",
+    "url": "/static/js/main.b2cd7947.chunk.js"
+  },
+  {
+    "revision": "82028d059a5d384db23a",
+    "url": "/static/js/runtime-main.d2782356.js"
+  }
+]);
