30a468063311eb460750dc9d9fb5ccfa032aa5c6
Steven Otalvaro <stevenotalvaroz@gmail.com>
2021-06-29 16:02:49 -0500

    useAyncCallback implemented

--- src/components/PokemonInfo.js
+++ src/components/PokemonInfo.js
@@ -1,32 +1,68 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useReducer} from 'react'
 import {fetchPokemon} from '../hooks/fetchPokemon'
 import {PokemonDataView} from './PokemonDataView'
 import {PokemonInfoFallback} from './PokemonInfoFallback'
 
-export function PokemonInfo({pokemonName}) {
-  const [state, setState] = useState({
-    status: pokemonName ? 'pending' : 'idle', // there was a little refresh when already have a pokemon loaded, if we will make a new petition that pokemon refresh the component
-    pokemon: null,
+function pokemonInfoReducer(state, action) {
+  switch (action.type) {
+    case 'pending': {
+      return {status: 'pending', data: null, error: null}
+    }
+
+    case 'resolved': {
+      return {status: 'resolved', data: action.data, error: null}
+    }
+
+    case 'rejected': {
+      return {status: 'rejected', data: null, error: action.error}
+    }
+
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`)
+    }
+  }
+}
+
+function usePokemonAyncReducer(asyncCallback, initialState, dependencies) {
+  const [state, dispatch] = useReducer(pokemonInfoReducer, {
+    status: 'idle', // there was a little refresh when already have a pokemon loaded, if we will make a new petition that pokemon refresh the component
+    data: null,
     error: null,
+    ...initialState,
   })
 
-  const {pokemon, error, status} = state // the destructuration, I could have done it directly at useState, but I think to see better this way
-
   useEffect(() => {
-    if (!pokemonName) {
+    const promise = asyncCallback()
+    if (!promise) {
       return
     }
 
-    setState({status: 'pending'})
-    fetchPokemon(pokemonName).then(
-      pokemon => {
-        setState({status: 'resolved', pokemon})
+    dispatch({type: 'pending'})
+    promise.then(
+      data => {
+        dispatch({type: 'resolved', data})
       },
       error => {
-        setState({status: 'rejected', error})
+        dispatch({type: 'rejected', error})
       },
     )
-  }, [pokemonName])
+  }, dependencies)
+  return state
+}
+
+export function PokemonInfo({pokemonName}) {
+  const state = usePokemonAyncReducer(
+    () => {
+      if (!pokemonName) {
+        return
+      }
+      return fetchPokemon(pokemonName)
+    },
+    {status: pokemonName ? 'pending' : 'idle'},
+    [pokemonName],
+  )
+
+  const {data: pokemon, error, status} = state // the destructuration, I could have done it directly at useState, but I think to see better this way
 
   // there is a most logic about the show the user, so implemented a state for handle our components
   if (status === 'idle') {
